3e229cee9be0100cc8b8897c8cb93b6e9aad0e5c
Mark <mark.aaron.fisher@gmail.com>
2019-03-29 13:09:02 -0700

    update lodash and change cypress folder structure

---
+++ cypress/integration/myPCC/d2L_spec.js
@@ -0,0 +1,9 @@
+// Cypress.on('fail', (err, runnable) => {
+//   debugger
+// });
+
+describe('Automates file additions to D2L content page', function() {
+  it('logs in', function(){
+    cy.visit('https://my.pcc.edu');
+  });
+ });
